2a5b434068823e9b2fcc66ea8eca8bbc9efe9a19
rthaworn <ratchapol.thaworn@agoda.com>
2025-04-05 23:06:09 +0700

    Init pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { Routes } from "@generouted/react-router";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(<Routes />);
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import type { FunctionComponent } from "react";
+
+const Home: FunctionComponent = () => {
+  return <h1>Yo</h1>;
+};
+
+export default Home;
