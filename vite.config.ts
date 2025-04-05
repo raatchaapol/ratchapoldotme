218ba7ad8798c790a5f690dfcaf648f1ec463543
rthaworn <ratchapol.thaworn@agoda.com>
2025-04-06 00:45:32 +0700

    Init chakra UI

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import path from 'node:path'
 import generouted from '@generouted/react-router/plugin'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig, loadEnv } from 'vite'
+import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
-    plugins: [react(), generouted()],
+    plugins: [react(), generouted(), tsconfigPaths()],
     server: {
       host: env.VITE_HOST,
       port: mode === 'development' ? parseInt(env.VITE_DEV_PORT) : parseInt(env.VITE_PRD_PORT)
